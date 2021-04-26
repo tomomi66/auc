18415b9d844736e00ae715fb683b789c55df5b90
tomomi66 <nishioka@zero-6.net>
2021-04-26 17:28:36 +0900

    change migration file carTabel

--- database/migrations/2021_04_26_170737_create_cars_table.php
+++ database/migrations/2021_04_26_170737_create_cars_table.php
@@ -15,6 +15,19 @@ class CreateCarsTable extends Migration
     {
         Schema::create('cars', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('name');
+            $table->integer('in_number')->index('入庫番号');
+            $table->integer('status')->index('状態');
+            $table->integer('registration_volume')->index('登録パーツ数');
+            $table->string('made_date')->index('年式');
+            $table->string('model_type')->index('型式');
+            $table->string('model_grade')->index('グレード');
+            $table->string('color')->index('色');
+            $table->integer('color_no')->index('カラーNo');
+            $table->integer('trim_no')->index('トリムナンバー');
+            $table->integer('mileage')->index('走行距離');
+            $table->string('create_parson')->index('新規作成者');
+            $table->string('chenge_person')->index('変更者');
             $table->timestamps();
         });
     }
