1e7eb04685c1b8967760c96647b7755ca922acdf
tomomi66 <nishioka@zero-6.net>
2021-04-26 17:32:38 +0900

    マイグレーションファイル修正　carTable

--- database/migrations/2021_04_26_170737_create_cars_table.php
+++ database/migrations/2021_04_26_170737_create_cars_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use PhpParser\Comment;
 
 class CreateCarsTable extends Migration
 {
@@ -16,18 +17,18 @@ class CreateCarsTable extends Migration
         Schema::create('cars', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
-            $table->integer('in_number')->index('入庫番号');
-            $table->integer('status')->index('状態');
-            $table->integer('registration_volume')->index('登録パーツ数');
-            $table->string('made_date')->index('年式');
-            $table->string('model_type')->index('型式');
-            $table->string('model_grade')->index('グレード');
-            $table->string('color')->index('色');
-            $table->integer('color_no')->index('カラーNo');
-            $table->integer('trim_no')->index('トリムナンバー');
-            $table->integer('mileage')->index('走行距離');
-            $table->string('create_parson')->index('新規作成者');
-            $table->string('chenge_person')->index('変更者');
+            $table->integer('in_number')->comment('入庫番号');
+            $table->integer('status')->comment('状態');
+            $table->integer('registration_volume')->comment('登録パーツ数');
+            $table->string('made_date')->comment('年式');
+            $table->string('model_type')->comment('型式');
+            $table->string('model_grade')->comment('グレード');
+            $table->string('color')->comment('色');
+            $table->integer('color_no')->comment('カラーNo');
+            $table->integer('trim_no')->comment('トリムナンバー');
+            $table->integer('mileage')->comment('走行距離');
+            $table->string('create_parson')->comment('新規作成者');
+            $table->string('chenge_person')->comment('変更者');
             $table->timestamps();
         });
     }
