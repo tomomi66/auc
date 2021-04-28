ab089768a43574ae95d4aed43630f67ebc66209a
tomomi66 <nishioka@zero-6.net>
2021-04-28 17:05:59 +0900

    TOPページ追加変更

---
+++ resources/views/top.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>ヤフオク登録</title>
+  </head>
+  <body>
+    <div class="container">
+		<h1>ヤフオク登録</h1>
+		<div class="col-12">
+			
+			<div class="general">
+				<h3>車輌登録</h3>
+				<a href="car_regist.html"><button type="button" class="btn btn-primary btn-lg btn-lg">車輌登録</button></a>
+				<a href="car_list.html"><button type="button" class="btn btn-primary btn-lg btn-lg">車輌一覧</button></a>
+			</div>
+			
+			<div class="general">
+				<h3>ヤフオク商品</h3>
+				<a href="product_list.html"><button type="button" class="btn btn-primary btn-lg btn-lg">商品一覧</button></a
+			</div>
+
+			
+			<div class="general">
+				<h3>ヤフオク基本設定</h3>
+				<button type="button" class="btn btn-primary btn-lg btn-lg">ヤフオク基本設定</button>
+			</div>
+			
+			<div class="general">
+				<h3>システム設定</h3>
+				<button type="button" class="btn btn-primary btn-lg btn-lg">ユーザー登録</button>
+				<button type="button" class="btn btn-primary btn-lg btn-lg">ユーザー一覧</button>
+			</div>
+		</div>
+	</div>
+  </body>
+</html>
