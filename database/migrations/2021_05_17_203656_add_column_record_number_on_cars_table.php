7a6955e5d8a3b34ed24efbecb2fcdc727c9b350e
tomomi66 <nishioka@zero-6.net>
2021-05-17 20:45:04 +0900

    carsテーブルのカラム変更 カルテ番号・シフト・年式（年）・年式（月）のカラム追加 不要なカラムの削除　年式・登録パーツ数

---
+++ database/migrations/2021_05_17_203656_add_column_record_number_on_cars_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddColumnRecordNumberOnCarsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('cars', function (Blueprint $table) {
+            $table->string('record_number')->comment('カルテ番号');
+            $table->string('gear_shift')->comment('シフト');
+            $table->integer('made_year')->comment('年式-年');
+            $table->integer('made_month')->comment('年式-月');
+
+            $table->dropColumn('registration_volume');
+            $table->dropColumn('made_date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('cars', function (Blueprint $table) {
+            $table->dropColumn('record_number');
+            $table->dropColumn('gear_shift');
+            $table->dropColumn('made_year');
+            $table->dropColumn('made_month');
+            $table->integer('registration_volume')->comment('登録パーツ数');
+            $table->string('made_date')->comment('年式');
+        });
+    }
+}
